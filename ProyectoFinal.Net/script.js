d73729711faff627187bbdd49edc4f29756d8954
pedrojose <kettdiva@gmail.com>
2024-06-01 18:21:37 +0200

    Casa2

--- ProyectoFinal.Net/script.js
+++ ProyectoFinal.Net/script.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Código para el conversor de divisas
+
+    const monedasList = document.getElementById('monedas-list');
+
+    fetch('https://v6.exchangerate-api.com/v6/f8c975868ee6a437db59716d/latest/USD')
+        .then(response => response.json())
+        .then(data => {
+            if (data.result === 'success') {
+                const conversionRates = data.conversion_rates;
+                for (const currency in conversionRates) {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = currency;
+                    monedasList.appendChild(listItem);
+                }
+            } else {
+                console.error('Hubo un error al cargar las monedas:', data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
     const fromCurrencySelect = document.getElementById('from-currency');
     const toCurrencySelect = document.getElementById('to-currency');
 
@@ -51,22 +71,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     console.error('Error:', error);
                     document.getElementById('result').textContent = 'Hubo un error en la conversión.';
                 });
-        });
-    }
-
-    // Código para el formulario de contacto
-    const contactForm = document.getElementById('contact-form');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(event) {
-            event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            const message = document.getElementById('message').value;
-
-            // Aquí podrías agregar la lógica para enviar el formulario a un servidor
-            // Para este ejemplo, simplemente mostraremos un mensaje de éxito
-            document.getElementById('form-response').textContent = `Gracias, ${name}. Hemos recibido tu mensaje y nos pondremos en contacto contigo pronto.`;
-            contactForm.reset(); // Limpia el formulario
         });
     }
 });
